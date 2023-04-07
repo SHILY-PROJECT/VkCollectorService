691c214460af5a54ddeb0a3eea2382de419174ff
SHILY <iliyarock@gmail.com>
2023-04-07 22:48:01 +0300

    Added model

---
+++ src/VkMarketParser/VkMarketClientConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace VkMarketParser;
+
+public class VkMarketClientConfiguration
+{
+    public ulong AppId { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public string AccessToken { get; set; }
+}
