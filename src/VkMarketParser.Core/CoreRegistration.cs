d4536f6cff21c724f86dab2f9f5a93d5a9ee48b9
SHILY <iliyarock@gmail.com>
2023-04-08 00:21:08 +0300

    Added configuration for dependency injection

---
+++ src/VkMarketParser.Core/CoreRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VkMarketParser.Core.VkMarket;
+using VkNet;
+
+namespace VkMarketParser.Core;
+
+public static class CoreRegistration
+{
+    public static IServiceCollection AddCore(this IServiceCollection service)
+    {
+        service
+            .AddSingleton<IVkMarketClientConfiguration, VkMarketClientConfiguration>()
+            .AddScoped<IVkMarketClient, VkMarketClient>()
+            .AddScoped<VkApi>();
+        
+        return service;
+    }
+}
---
+++ src/VkMarketParser.Ui/UiRegistration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VkMarketParser.Core.VkMarket;
+
+namespace VkMarketParser;
+
+public static class UiRegistration
+{
+    public static IServiceCollection AddUi(this IServiceCollection services)
+    {
+        services
+            .AddSingleton(ConfigurationFactory)
+            .AddSingleton(VkMarketClientConfigurationFactory)
+            .AddSingleton<Program>();
+        
+        return services;
+    }
+    
+    private static IConfiguration ConfigurationFactory() => new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", true, true)
+        .AddUserSecrets<Startup>()
+        .Build();
+
+    private static IVkMarketClientConfiguration VkMarketClientConfigurationFactory(IServiceProvider services)
+    {
+        var config = services.GetRequiredService<IConfiguration>();
+        var vkConfig = config.GetSection("VkMarketClientConfiguration").Get<VkMarketClientConfiguration>();
+        if (vkConfig is null) throw new InvalidCastException();
+        return vkConfig;
+    }
+}
