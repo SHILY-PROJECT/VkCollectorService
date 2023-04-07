a75b82cf30a604319397c10208b30d7b466f0564
SHILY <iliyarock@gmail.com>
2023-04-08 00:19:00 +0300

    Added 'Program'

---
+++ src/VkMarketParser.Ui/Program.cs
@@ -0,0 +1,21 @@
+﻿using VkMarketParser.Core.VkMarket;
+
+namespace VkMarketParser;
+
+public class Program
+{
+    private readonly IVkMarketClient _client;
+    
+    public Program(IVkMarketClient vkMarketClient)
+    {
+        _client = vkMarketClient;
+    }
+    
+    public async Task RunAsync()
+    {
+        var groupLink = "";
+        
+        await _client.AuthorizeAsync();
+        var products = await _client.GetProductsAsync(groupLink, 500);
+    }
+}
