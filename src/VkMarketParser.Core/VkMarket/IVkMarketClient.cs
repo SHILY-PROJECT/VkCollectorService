6cd0285750880161b167a8a25d8dd08ac2fb24aa
SHILY <iliyarock@gmail.com>
2023-04-08 00:19:31 +0300

    Added interfaces

---
+++ src/VkMarketParser.Core/VkMarket/IVkMarketClient.cs
@@ -0,0 +1,9 @@
+﻿using VkNet.Model;
+
+namespace VkMarketParser.Core.VkMarket;
+
+public interface IVkMarketClient
+{
+    Task AuthorizeAsync();
+    Task<List<Market>> GetProductsAsync(string groupLink, int maxCount);
+}
---
+++ src/VkMarketParser.Core/VkMarket/IVkMarketClientConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace VkMarketParser.Core.VkMarket;
+
+public interface IVkMarketClientConfiguration
+{
+    ulong AppId { get; set; }
+    string Login { get; set; }
+    string Password { get; set; }
+    string AccessToken { get; set; }
+}
