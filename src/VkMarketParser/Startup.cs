12eb4839d04d0c3d4d78ec89639bc0bd1ab30c38
SHILY <iliyarock@gmail.com>
2023-04-07 22:49:04 +0300

    Renaming

---
+++ src/VkMarketParser/Startup.cs
@@ -0,0 +1,14 @@
+﻿using VkMarketParser;
+
+var groupLink = "";
+var config = new VkMarketClientConfiguration
+{
+    AppId = 0,
+    Login = "",
+    Password = ""
+};
+var client = new VkMarketClient(config);
+await client.AuthorizeAsync();
+var products = await client.GetProductsAsync(groupLink, 500);
+
+Console.ReadKey();
