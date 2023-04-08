538cf479e8d0b4042f4ebe15c61cf675167e26fa
SHILY <iliyarock@gmail.com>
2023-04-08 21:18:34 +0300

    Added load settings last version

--- src/VkMarketParser.Core/VkMarket/VkMarketClientConfigurationFactory.cs
+++ src/VkMarketParser.Core/VkMarket/VkMarketClientConfigurationFactory.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Text;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using VkMarketParser.Core.VkMarket;
 
 namespace VkMarketParser.Core;
@@ -9,8 +12,17 @@ public static class VkMarketClientConfigurationFactory
     public static IVkMarketClientConfiguration Create(IServiceProvider services)
     {
         var config = services.GetRequiredService<IConfiguration>();
-        var vkConfig = config.GetSection("VkMarketClientConfiguration").Get<VkMarketClientConfiguration>();
+        var envConfig = services.GetRequiredService<EnvironmentConfiguration>();
+        var mapper = services.GetRequiredService<IMapper>();
+        var vkConfig = config.GetSection(nameof(VkMarketClientConfiguration)).Get<VkMarketClientConfiguration>();
+        
         if (vkConfig is null) throw new InvalidCastException();
+        
+        if (File.Exists(envConfig.SettingsLastVersionFileName) && JsonConvert.DeserializeObject<VkMarketClientConfiguration>(File.ReadAllText(envConfig.SettingsLastVersionFileName, Encoding.UTF8)) is { } vkConfigLastVer)
+        {
+            mapper.Map(vkConfigLastVer, vkConfig);
+        }
+        
         return vkConfig;
     }
 }
